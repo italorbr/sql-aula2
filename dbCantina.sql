8e0b642713461a23ec4597634eb2a089cac3494c
italorbr <italorosa.profissional@gmail.com>
2024-08-09 20:10:40 -0300

    mais tabelas

--- dbCantina.sql
+++ dbCantina.sql
@@ -16,8 +16,15 @@ create table tbFuncionarios(
     telCel char(10),
     primary key (codFunc)
 );
--- visualizando tabelas
-desc tbFuncionarios;
+
+create table tbFornecedores(
+    codForn int not null auto_increment,
+    nome varchar(100) not null,
+    cnpj char(17) not null unique,
+    email varchar(100) not null,
+    primary key (codForn)
+);
+
 
 create table tbUsuarios(
     codUsu int not null auto_increment,
@@ -28,4 +35,22 @@ create table tbUsuarios(
     foreign key(codFunc) references tbFuncionarios(codFunc) -- chave estrangeira, tem que ter uma chave de msm nome ja criada **
 );
 
+
+create table tbProdutos(
+    codProd int not null auto_increment,
+    descricao varchar(100),
+    quantidade int,
+    valor decimal(9,2),
+    validade date,
+    dataEntrada date,
+    horaEntrada time,
+    codForn int not null,
+    primary key (codProd),
+    foreign key(codForn) references tbFornecedores(codForn)
+);
+
+-- visualizando tabelas
+desc tbFuncionarios;
 desc tbUsuarios;
+desc tbFornecedores;
+desc tbProdutos;
