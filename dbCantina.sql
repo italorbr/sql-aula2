cc3d383d1bab63d7d224e0596f9696afcc86f4fb
italorbr <italorosa.profissional@gmail.com>
2024-08-09 19:55:12 -0300

    segunda tabela

--- dbCantina.sql
+++ dbCantina.sql
@@ -11,8 +11,21 @@ create table tbFuncionarios(
     email varchar(100) unique,
     cpf char(14) not null unique,
     sexo char(1) default "F" check(sexo in('F' , 'M')), -- default é pra colocar algo caso deixem vazio o check é pra entrar so f ou m 
-    salario decimal(9,2),
+    salario decimal(9,2) default 0 check(salario >= 0),
     nascimento date,
     telCel char(10),
     primary key (codFunc)
 );
+-- visualizando tabelas
+desc tbFuncionarios;
+
+create table tbUsuarios(
+    codUsu int not null auto_increment,
+    nome varchar(25) not null unique,
+    senha varchar(10) not null unique,
+    codFunc int not null, -- ** essa chave aqui
+    primary key(codUsu),
+    foreign key(codFunc) references tbFuncionarios(codFunc) -- chave estrangeira, tem que ter uma chave de msm nome ja criada **
+);
+
+desc tbUsuarios;
